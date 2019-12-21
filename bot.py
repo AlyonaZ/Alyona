0c780277ae2b16ba1dc472024e4b6b6822232aca
AlyonaZ <58879508+AlyonaZ@users.noreply.github.com>
2019-12-21 19:06:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,11 @@
 import vk_api
 import requests
 import random
-import os
 from bs4 import BeautifulSoup 
 from vk_api.longpoll import VkLongPoll, VkEventType
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
-token = 'bot-token'
+token = '2510043a6191f2796a42b3c3c9a7e85ba77c66c2c5636d087fe3be87b97f8cdab271bdeac986eabfecfe5'
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
 for event in longpoll.listen():
